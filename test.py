4697fb421711516c4005ed8201538837941e0de2
zhuDmi <zhu@me.com>
2022-12-12 21:25:30 +0700

    add branch HW2

--- test.py
+++
@@ -1,8 +0,0 @@
-import joblib
-import pandas as pd
-import userknn
-
-interactions = pd.read_csv('/Users/dmitry/PycharmProjects/recsys/data/datasets/interactions_processed.csv')
-model = joblib.load('/Users/dmitry/PycharmProjects/recsys/data/models/userknn_tined.joblib')
-
-x = model.predict(704055, 10)
